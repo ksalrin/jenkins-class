2695da5f6b543945625e640b2b495ede9ebf9471
ksalrin <salrindesigngeek@gmail.com>
2020-09-08 23:51:07 +0000

    updated

--- JenkinsBuilder.groovy
+++ JenkinsBuilder.groovy
@@ -44,6 +44,9 @@ def slavePodTemplate = """
             }
           
           dir('deployments/docker') {
+                stage("Docker Build"){
+                    sh 'docker build -t artemis'
+                }
                 stage('checking') {
                     sh 'ls -l'
                 }
