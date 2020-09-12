b283d3255dc27eea645a72a883248404b3c61792
ksalrin <salrindesigngeek@gmail.com>
2020-09-12 21:11:03 +0000

    deploy add stages

--- JenkinsDeploy.groovy
+++ JenkinsDeploy.groovy
@@ -8,7 +8,60 @@ properties([
         ])
         ])
 
-println(
+
+// Uniq name for the pod or slave 
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+
+//yaml def for slaves
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+      node(k8slabel) {
+
+        stage("Pull the SCM"){
+            git 'https://github.com/ksalrin/jenkins-class.git'
+        }  
+
+        stage("Apply/Plan") {
+            container("fuchicorptools") {
+                sh 'kubectl version'
+            }
+        }
+      }
+    }
+
+    println(
     """
     Apply changes: ${params.applyChanges}
     Destroy changes: ${params.destroyChanges}
